5689d385749f14668572c20a0fcc0bb4a4d03196
Chris Tessum <ctessum@gmail.com>
2021-06-09 20:47:58 -0700

    Add initial code

--- src/Grader.jl
+++ src/Grader.jl
@@ -1,5 +1,93 @@
 module Grader
 
-# Write your package code here.
+using Parameters
+import Random
+
+@with_kw mutable struct Image
+    label::String = ""
+    url::String = ""
+end
+
+@with_kw mutable struct TestResult
+    name::String = ""
+    description::String = ""
+    points::Int = 0
+    max_points::Int = 0
+    message::String = ""
+    output::String = ""
+    images::Vector{Image} = []
+end
+
+@with_kw mutable struct Problem
+    gradable::Bool = true
+    score::AbstractFloat = 0
+    message::String = ""
+    output::String = ""
+
+    images::Vector{Image} = []
+
+    tests::Vector{TestResult} = []
+end
+
+# Evaluate the provided code string inside a module,
+# and return the resulting module. May not 
+# work if expected if the code string already is a module.
+function evalasmodule(code::AbstractString)
+    mod = "module "*Random.randstring(Random.MersenneTwister(), "abcdefghijklmnopqrstuvwxyz", 20)*"\n" * code * "\nend"
+    expr = Meta.parse(mod)
+    eval(expr)
+end
+
+function run!(p::Problem, goldencode::AbstractString, studentcode::AbstractString)
+    goldenresult = Module()
+    studentresult = Module()
+    try
+        goldenresult = evalasmodule(goldencode)
+    catch err
+        p.output = p.output * "error running golden code:\n"*sprint(showerror, err, backtrace()) * "\n"
+        p.gradable = false
+        p.message = "Internal grading error, please notify instructor."
+    end
+
+    try
+        studentresult = evalasmodule(studentcode)
+    catch err
+        p.output = p.output * "error running student code:\n"*sprint(showerror, err, backtrace()) * "\n"
+        p.gradable = false
+        p.message = "There was an error running your code, please see information below."
+    end
+    return (goldenresult, studentresult)
+end
+
+function grade!(p::Problem, name::String, description::String, points::Int, expr::Expr, msg_if_incorrect::String)
+    if !p.gradable 
+        return nothing
+    end
+    
+    t = TestResult(max_points=points, name=name, description=description)
+
+    correct = false
+    try 
+        correct = eval(expr)
+    catch err
+        if isa(err, UndefVarError)
+            t.output = t.output * "\n" * sprint(showerror, err)
+        else
+            rethrow(err)
+        end
+    end
+    correct ? t.points = points : t.message = msg_if_incorrect
+    
+    append!(p.tests, [t])
+
+    pts = 0
+    totalpts = 0
+    for t in p.tests
+        pts += t.points
+        totalpts += t.max_points
+    end
+    p.score = float(pts) / float(totalpts)
+    return nothing
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,129 @@ using Grader
 using Test
 
 @testset "Grader.jl" begin
-    # Write your tests here.
+    # Test whether evalasmodule is working correctly for a basic problem.
+    @testset "evalasmodule" begin
+        golden_code = """x=2
+        y = x * 3
+        """
+
+        student_code = """x=2
+        y = x + x + x
+        z = 4
+        """
+
+        golden_result = evalasmodule(golden_code)
+        student_result = evalasmodule(student_code)
+
+
+        @testset "correct x" begin 
+            @test golden_result.x == student_result.x
+        end
+        @testset "correct y" begin 
+            @test golden_result.y ≈ student_result.y
+        end    
+        @testset "correct z only in student code" begin
+            @test_throws UndefVarError golden_result.z
+            @test student_result.z == 4
+        end
+
+        @testset "Throws ParseError for syntax error" begin
+            syntaxerr = "x= @@@"
+            @test_throws Base.Meta.ParseError evalasmodule(syntaxerr)
+        end
+    end
+
+    @testset "grade problem" begin
+        @testset "correct answer" begin
+            goldencode = """x=2
+            y = x * 3
+            """
+            studentcode = """x=2
+            y = x + x + x
+            """
+
+            p = Problem()
+            golden, student = run!(p, goldencode, studentcode)
+
+            grade!(p, "y", "check y", 2, :($student.y ≈ $golden.y), "y is incorrect")
+
+            @test length(p.tests) == 1
+            @test p.tests[1].max_points == 2
+            @test p.tests[1].points == 2
+            @test p.score ≈ 1
+        end
+
+        @testset "partially correct answer" begin
+            goldencode = """x=2
+            y = x * 3
+            z = y + 2
+            """
+            studentcode = """x=2
+            y = x + x + x
+            z = y + 3
+            """
+
+            p = Problem()
+            golden, student = run!(p, goldencode, studentcode)
+
+            grade!(p, "y", "check y", 2, :($student.y ≈ $golden.y), "y is incorrect")
+            grade!(p, "z", "check z", 8, :($student.z ≈ $golden.z), "z is incorrect")
+
+            @test length(p.tests) == 2
+            @test p.tests[1].max_points == 2
+            @test p.tests[2].max_points == 8
+            @test p.tests[1].points == 2
+            @test p.tests[2].points == 0
+            @test p.tests[2].message == "z is incorrect"
+            @test p.score ≈ 0.2
+        end
+
+        @testset "error in student code" begin
+            goldencode = """x=2
+            y = x * 3
+            """
+            studentcode = """x=2
+            y = x + x + xx
+            """
+
+            p = Problem()
+            golden, student = run!(p, goldencode, studentcode)
+
+            grade!(p, "y", "check y", 2, :($student.y ≈ $golden.y), "y is incorrect")
+
+            @test !p.gradable
+            @test p.message == "There was an error running your code, please see information below."
+        end
+
+        @testset "error in golden code" begin
+            goldencode = """x=2
+            y = x * 3y
+            """
+            studentcode = """x=2
+            y = x + x + x
+            """
+
+            p = Problem()
+            golden, student = run!(p, goldencode, studentcode)
+
+            @test !p.gradable
+            @test p.message == "Internal grading error, please notify instructor."
+        end
+
+        @testset "student missing variable" begin
+            goldencode = """x=2
+            y = x * 3
+            """
+            studentcode = """x=2
+            """
+
+            p = Problem()
+            golden, student = run!(p, goldencode, studentcode)
+
+            grade!(p, "y", "check y", 2, :($student.y ≈ $golden.y), "y is incorrect")
+
+            @test p.tests[1].output == "\nUndefVarError: y not defined"
+            @test p.tests[1].message == "y is incorrect"
+        end
+    end
 end
